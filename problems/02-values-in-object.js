4860cb8ef8874eafa604251512f24a5f441d80ea
Kyle Tseng <ktseng122@gmail.com>
2021-03-16 17:37:07 -0400

    02 clear

--- problems/02-values-in-object.js
+++ problems/02-values-in-object.js
@@ -1,6 +1,6 @@
 /***********************************************************************
-Write a function called `valuesInObject(obj)` that takes in an object and returns 
-an array of all the values within that Object. 
+Write a function called `valuesInObject(obj)` that takes in an object and returns
+an array of all the values within that Object.
 
 
 Do this once using using a `for...in` loop and once using `Object.values`.
@@ -13,9 +13,23 @@ let foods = {apple: "tart", lemon: "sour", mango: "sweet"}
 valuesInObject(animals); // => ["Wolfie", "Jet", "Bilbo"]
 valuesInObject(foods); // => ["tart", "sour", "sweet"]
 ***********************************************************************/
+//Understand
+  //function valuesInObject
+    //accepts an object (obj)
+  //returns an ARRAY of all values within that object
 
+//Plan
+  //for loop that runs as long as # of keys
+    //assign key's values as a variable
+    //push that variable into array
+  //return array
 function valuesInObject(obj) {
-  // your code here
+  let array = [];
+  for(let key in obj){
+    //let value = obj[key]
+    array.push(obj[key]);
+  }
+  return array;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
