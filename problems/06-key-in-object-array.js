96a82298b4cc1650f882323d7aae650ab253833f
Kyle Tseng <ktseng122@gmail.com>
2021-03-16 19:49:27 -0400

    Complete 06

--- problems/06-key-in-object-array.js
+++ problems/06-key-in-object-array.js
@@ -1,7 +1,7 @@
 /***********************************************************************
-Write a function `keyInObjectArray(objArray, keyString)` that takes in an array 
-of objects as the first parameter and a string as the second. The 
-`keyInObjectArray` function will return `true` if any of the objects contains 
+Write a function `keyInObjectArray(objArray, keyString)` that takes in an array
+of objects as the first parameter and a string as the second. The
+`keyInObjectArray` function will return `true` if any of the objects contains
 the `keyString` as a key within them, and `false` if not.
 
 
@@ -18,10 +18,29 @@ keyInObjectArray(objArray, 'food'); // => false
 keyInObjectArray(objArray, 'animal'); // => false
 
 ***********************************************************************/
+//Understand
+  //function named keyInObjectArray
+    //accepts an array (objArray) and a string (keyString)
+  //return true if any of the objects contains the keyString
+  //return false if none contain keyString
 
+//Plan
+  //for loop to run until objArray.length
+    //have access to objects
 function keyInObjectArray(objArray, keyString) {
-  // your code here
-}
+  let found = false;
+  objArray.forEach(function(obj){
+  //for(i = 0; i < objArray.length; i++){
+    if (obj[keyString] !== undefined) {
+      found = true;
+    }
+      // if(keyString in objArray[i]){
+      //   return true;
+      // }
+      // else{return false;}
 
+  });
+  return found;
+}
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keyInObjectArray;
