52eb29d324ea7071eeb08822252a0ad907da086d
Kyle Tseng <ktseng122@gmail.com>
2021-03-16 18:09:35 -0400

    05 Clear

--- problems/05-does-key-exist.js
+++ problems/05-does-key-exist.js
@@ -9,9 +9,18 @@ let obj1 = {bootcamp: 'App Academy', course: 'Bootcamp Prep'}
 doesKeyExist(obj1, 'course'); // => true
 doesKeyExist(obj1, 'name'); // => false
 ***********************************************************************/
-
+//Understand
+  //function named doesKeyExist
+    //accept a object (obj) and a string (key)
+//Plan
+  //if (key in obj)
+    //return true
+  //else return false;
 function doesKeyExist(obj, key) {
-  // your code here
+  if(key in obj){
+    return true;
+  }
+  return false;
 }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
